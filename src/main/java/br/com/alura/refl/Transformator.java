f5a40c53dd22597620b66827c237f12414bd7a24
Zanellas <zanellas@outlook.com>
2025-02-03 01:16:02 -0400

    Aula 03

--- src/main/java/br/com/alura/refl/Transformator.java
+++ src/main/java/br/com/alura/refl/Transformator.java
@@ -3,6 +3,7 @@ package br.com.alura.refl;
 import java.lang.Class;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 
 public class Transformator {
 
@@ -15,6 +16,23 @@ public class Transformator {
         Field[] sourceFields = source.getDeclaredFields();
         Field[] targetFields = target.getDeclaredFields();
 
+        Arrays.stream(sourceFields).forEach(sourceField ->
+                Arrays.stream(targetFields).forEach(targetField -> {
+                    validate(sourceField, targetField);
+                    try {
+                        targetField.set(targetClass, sourceField.get(input));
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                }));
+
         return targetClass;
+    }
+
+    private void validate(Field sourceField, Field targetField) {
+        if (sourceField.getName().equals(targetField.getName()) && sourceField.getType().equals(targetField.getType())) {
+            sourceField.setAccessible(true);
+            targetField.setAccessible(true);
+        }
     }
 }
